883611b92c01a3153fb8a7e833764cb34e19ea02
root <brian_chuk@yahoo.com>
2017-11-03 01:02:12 -0400

    header complete

---
+++ res/js/menu_nav.dev.js
@@ -0,0 +1,115 @@
+fadeIn(document.getElementsByClassName('fade-content')[0],1000)
+
+// higher speed is slower fadeIn
+function fadeIn(el, speed) {
+    if (speed === undefined) {
+        speed = 400;
+    }
+
+    el.style.opacity = 0;
+    var last = +new Date();
+    var tick = function() {
+        el.style.opacity = +el.style.opacity + (new Date() - last) / speed;
+        last = +new Date();
+
+        if (+el.style.opacity < 1) {
+            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+        }
+    };
+
+    tick();
+}
+
+function fadeOut(el, speed) {
+    if (speed === undefined) {
+        speed = 400;
+    }
+
+    el.style.opacity = 1;
+    var last = +new Date();
+    var tick = function() {
+        el.style.opacity = +el.style.opacity - (new Date() - last) / speed;
+        last = +new Date();
+
+        if (+el.style.opacity > 0) {
+            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+        }
+    };
+
+    tick();
+}
+
+function toggleClass(el, className) {
+    if (el.classList) {
+        el.classList.toggle(className);
+    } else {
+        var classes = el.className.split(' ');
+        var existingIndex = classes.indexOf(className);
+
+        if (existingIndex >= 0)
+            classes.splice(existingIndex, 1);
+        else
+            classes.push(className);
+
+        el.className = classes.join(' ');
+    }
+}
+
+var showNav = false;
+var menuButton = document.getElementById('menu-icon');
+var navigation = document.getElementById('navigation');
+var listItems = navigation.children[0].children;
+
+menuButton.onclick = function () {
+    toggleClass(menuButton, 'is-active');
+    
+    if (showNav === false) {
+        navigation.style.display = 'block';
+        fadeIn(navigation, 200)
+        
+        setTimeout(function () {
+            for (var i = 0; i < listItems.length; i++) {
+                fadeIn(listItems[i], 100)
+            }
+        }, 300);
+    } else {
+        fadeOut(navigation, 200)
+        setTimeout(function () {
+            for (var i = 0; i < listItems.length; i++) {
+                listItems[i].style.opacity = 0;
+            }
+            navigation.style.display = 'none';
+        }, 300);
+    }
+
+    showNav = !showNav;
+};
+
+function displayOnDesktop () {
+    if (window.innerWidth > 900) {
+        navigation.style.display = 'block';
+        navigation.style.opacity = 1;
+
+        for (var i = 0; i < listItems.length; i++) {
+            listItems[i].style.opacity = 1;
+        }
+
+        showNav = false;
+        // toggleClass(menuButton, 'is-active');
+    } else if (window.innerWidth <= 900 && showNav === false) {
+        navigation.style.display = 'none';
+        navigation.style.opacity = 0;
+
+        for (var i = 0; i < listItems.length; i++) {
+            listItems[i].style.opacity = 0;
+        }
+
+        if (menuButton.classList) {
+            menuButton.classList.remove('is-active');
+        } else {
+            menuButton.className = menuButton.className.replace(new RegExp('(^|\\b)' + 'is-active'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        }
+    }
+} 
+
+window.addEventListener('resize', displayOnDesktop, false);
